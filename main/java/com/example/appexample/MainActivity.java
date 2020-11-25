6928692383a96d7958d843b0aac7ae55e40eb85f
DSilva21 <31378533+DSilva21@users.noreply.github.com>
2020-11-25 23:44:14 +0900

    Add files via upload

--- main/java/com/example/appexample/MainActivity.java
+++ main/java/com/example/appexample/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -20,8 +22,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View view) {
+        CheckBox whippedCreamCheckBox=(CheckBox)findViewById(R.id.whipped_cream_checkbox);
+        CheckBox chocolateCheckBox=(CheckBox)findViewById(R.id.chocolate_checkbox);
+        EditText nameField=(EditText) findViewById(R.id.name_field);
+        String name= nameField.getText().toString();
+        boolean hasWhippedCream=whippedCreamCheckBox.isChecked();
+        boolean haschocolate=chocolateCheckBox.isChecked();
         int price=calculatePrice();
-        displayMessage(createOrderSummary(price));
+        String priceMessage=createOrderSummary(name,price,hasWhippedCream,haschocolate);
+        displayMessage(priceMessage);
     }
 
     public void increment(View view) {
@@ -47,12 +56,16 @@ public class MainActivity extends AppCompatActivity {
         return quantity*5;
     }
 
-    private String createOrderSummary(int price)
+    private String createOrderSummary(String name, int price,boolean addwhippedCream, boolean addchocolate)
     {
-        String priceMessage="Name: kunal";
+        String priceMessage="Name: "+name;
+        priceMessage+="\nAdd whipped cream? "+addwhippedCream;
+        priceMessage+="\nAdd chocolate? "+addchocolate;
         priceMessage+="\nQuantity: "+quantity;
         priceMessage+="\nTotal: $"+price;
         priceMessage+="\nThank you";
         return priceMessage;
     }
+
+
 }
